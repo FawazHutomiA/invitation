0ece47f93e2d1ac86fdc787c6143fece1c2ff178
FawazHutomiA <hutomifawaza@gmail.com>
2023-10-06 14:35:49 +0700

    add: header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,38 @@
 <template>
-    <div class="bg-red-500 sticky top-0 h-20">
-        <h1>Ini Header</h1>
+  <div
+    class="sticky top-0 h-16 transition-bg duration-300 ease-in-out contianer px-36 flex items-center justify-between"
+    :class="{ 'shadow-md bg-white': scrolled, 'bg-transparent': !scrolled }"
+  >
+    <!-- Logo -->
+    <SvgLogo />
+    <!-- section right menu -->
+    <div class="flex items-center gap-x-6">
+      <span class="cursor-pointer text-[#171A31] text-sm">Home</span>
+      <span class="cursor-pointer text-[#171A31] text-sm">About us</span>
+      <span class="cursor-pointer text-[#171A31] text-sm">How it works</span>
+      <span class="cursor-pointer text-[#171A31] text-sm">Services</span>
+      <span class="cursor-pointer text-[#171A31] text-sm">Contact</span>
     </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      scrolled: false,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const scrollThreshold = 60;
+      this.scrolled = window.scrollY > scrollThreshold;
+    },
+  },
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,4 @@
 
-import TheHeader from '../components/TheHeader.vue';
 <template>
     <div class="h-screen flex flex-col justify-between">
         <div>
