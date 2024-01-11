6c372f5b3b6360b808a81b33af35196dceea6f6f
FawazHutomiA <hutomifawaza@gmail.com>
2024-01-11 13:51:02 +0700

    add: Initiate project

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 
 <template>
-    <div class="h-screen flex flex-col justify-between">
+    <!-- <div class="h-screen flex flex-col justify-between">
         <div>
-            <TheHeader />
             <slot />
         </div>
         <div class="bg-red-500">
             <h1>Footer</h1>
         </div>
-    </div>
+    </div> -->
+    <div>
+            <slot />
+        </div>
 </template>
---
+++ pages/[name]/[username].vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="relative">
+    <img src="https://einvite.id/wp-content/uploads/kajian-bg-fix-cover.jpg" alt="background" class="absolute -z-10 object-cover h-screen w-screen bg-center opacity-100 bg-[#FFF]">
+    <div class="flex flex-col items-center justify-center py-[4.5rem]">
+      <h1 class="font-nunito bg-clip-text text-transparent bg-gradient-to-r from-[#477D5A] to-[#727E67] text-[1.4rem]">Walimatul 'Ursy</h1>
+      <img src="/img/risman.png" alt="wedding" class="w-[14rem] p-4 bg-gradient-to-r from-[#477D5A] to-[#727E67] rounded-full mt-4">
+      <p class="font-croissant mt-4 text-[2rem] bg-clip-text text-transparent bg-gradient-to-r from-[#477D5A] to-[#727E67] font-semibold">{{ getName }}</p>
+      <p class="font-light tracking-wide text-[#477D5A] mt-12 text-[1.2rem]">Kepada Bapak/Ibu/Saudara/i:</p>
+      <p class="bg-clip-text text-transparent bg-gradient-to-r from-[#477D5A] to-[#727E67] font-semibold mt-4 text-[2rem]">{{ getNameInvitation }}</p>
+      <p class="font-light tracking-wide text-[#477D5A] mt-4 text-[1rem]">Tanpa mengurangi rasa hormat, kami bermaksud mengundang Anda untuk menghadiri acara pernikahan kami.</p>
+      <button class="bg-[#727E67] hover:bg-[#477D5A] transition-all transform ease-in-out duration-300 text-white rounded-md mt-4 px-3 py-2">Buka Undangan</button>
+      <p class="font-light tracking-wide text-[#477D5A] mt-4 text-[1rem]">* Mohon maaf apabila ada kesalahan penulisan nama/gelar</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    // Access the 'to' parameter from the URL
+    const toParam = this.$route;
+    console.log(toParam); // You can use this value as needed
+
+    // Your other mounted logic goes here
+  },
+  computed: {
+    getNameInvitation() {
+      const username = this.$route.params.username
+      return username.charAt(0).toUpperCase() + username.slice(1);
+    },
+    getName() {
+      const name = this.$route.params.name
+      const nameParts = name.split('-');
+      
+      // Capitalize each part
+      const capitalizedParts = nameParts.map(part => part.charAt(0).toUpperCase() + part.slice(1));
+      
+      // Join the parts with ' & '
+      return capitalizedParts.join(' & ');
+    }
+  }
+}
+</script>
+<style>
+.font-croissant {
+  font-family: 'Croissant One', serif;
+}
+.font-nunito {
+  font-family: 'Nunito', sans-serif;
+}
+</style>
